4bffdd7cb32753691aa3db598ea49522210caad5
Matthew Emond <me@ucf.edu>
2025-12-11 14:29:55 -0500

    crush pt. 1

--- app/core/migrations/0018_alter_question_data_rename_data_question__data.py
+++ app/core/migrations/0018_alter_question_data_rename_data_question__data.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0017_alter_widgetinstance_close_at_and_more"),
+        ("core", "0011_remove_usersettings_settings_and_more"),
     ]
 
     operations = [
